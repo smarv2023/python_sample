0abe87f08be48840711be4c2f7b107bb5ee591a8
Marvin <153880760+smarv2023@users.noreply.github.com>
2023-12-24 11:31:24 -0600

    udemy lesson added

---
+++ Udemy/hangman.py
@@ -0,0 +1,102 @@
+
+import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ / \\ |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\\ |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+life = len(stages) - 1
+
+# Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+# Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    # Check guessed letter
+    if guess in chosen_word:
+        for i in range(word_length):
+            if chosen_word[i] == guess:
+                display[i] = guess
+    else:
+        print(f"{stages[life]}")
+        life -= 1
+
+
+    # Then reduce 'lives' by 1.
+    # If lives goes down to 0 then the game should stop and it should print "You lose."
+
+    # Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    # Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    if life < 0:
+        end_of_game = True
+        print("You lose")
